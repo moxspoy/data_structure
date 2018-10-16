df247beccfed7c7abe27c221298910df6eb91315
moxspoy <mnurilmanbaehaqi@gmail.com>
2018-10-16 23:22:41 +0700

    another improvements

--- src/MyStackArray.java
+++ src/MyStackArray.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 /**
  * Created by MNurilmanBaehaqi on 10/16/2018.
  */
-public class MyStack {
+public class MyStackArray {
     public static void main(String[] ar) {
-        Stack <String> tumpuk = new Stack<String>();
-        Stack <String> tumpuk_backup = new Stack<>();
+        Tumpukan tumpuk = new Tumpukan();
         boolean flag = true;
         do {
             System.out.println("Pilih Operasi\n1. Masukkan data\n2. Tampilkan Data\n3. Ambil data\n0. Exit");
@@ -29,15 +27,13 @@ public class MyStack {
                 case 2:
                     System.out.println("Isi tumpukan:");
 
-                    Object[] vals = tumpuk.toArray();
-                    for(int i = vals.length - 1; i >= 0; i--)
-                    {
-
-                        System.out.println(vals[i]);
+                    while (! tumpuk.empty()) {
+                        String nama = tumpuk.pop();//keluarkan isi tumpukan
+                        System.out.println(nama);//tampilkan nama
                     }
                     break;
                 case 3:
-                    System.out.println(tumpuk.pop() + " diambil dari tumpukan");
+                    System.out.println(tumpuk.pop()+ " di ambil dari tumpukan");
                     break;
                 default:
                     flag = false;
@@ -45,5 +41,43 @@ public class MyStack {
                     break;
             }
         }while (flag);
+    }
+}
+
+class Tumpukan {
+    final int MAKSDATA = 8;
+    private String[] tumpukan = new String[MAKSDATA];
+    private int puncak;// Untuk mencatat puncak dlm tumpukan
+
+    // Konstruktor (langkah 1)
+    public Tumpukan() {
+        puncak = -1; //puncak= -1, berarti tumpukan kosong
+    }
+
+    public boolean push(String data) {
+        // Periksa kalau tumpukan penuh
+        if (puncak == (MAKSDATA - 1)) {
+            System.out.print("Tumpukan penuh! ");
+            System.out.println(data + " tidak dimasukkan.");
+
+            return false;
+        }
+        puncak++;
+        tumpukan[puncak] = data;
+        return true;
+    }
+
+    public String pop() {
+        String res = "";
+        if (!empty()) {
+            res = tumpukan[puncak--];
+            return res;
+        }
+        else
+            return "**error**";
+    }
+
+    public boolean empty() {
+        return (puncak == -1);
     }
 }
